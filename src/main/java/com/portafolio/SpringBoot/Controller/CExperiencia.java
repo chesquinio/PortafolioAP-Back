a686e68ffb237ed69231e45ba4ffd457fbe26d4a
francis <willenerfrancis0@gmail.com>
2022-11-03 15:52:14 -0300

    Modulo experiencia actualizado

--- src/main/java/com/portafolio/SpringBoot/Controller/CExperiencia.java
+++ src/main/java/com/portafolio/SpringBoot/Controller/CExperiencia.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,15 @@ public class CExperiencia {
         return new ResponseEntity(new Message("Experiencia actualizada"), HttpStatus.OK);
     }
     
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Experiencia> getById(@PathVariable("id") int id){
+        if(!sExperiencia.existsById(id))
+            return new ResponseEntity(new Message("No existe"), HttpStatus.NOT_FOUND);
+        Experiencia experiencia = sExperiencia.getOne(id).get();
+        return new ResponseEntity(experiencia, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
         //Validamos si existe el Id
         if(!sExperiencia.existsById(id))
