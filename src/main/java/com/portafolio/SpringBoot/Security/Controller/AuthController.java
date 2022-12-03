b6f4e299728de4fa92edd0ce787da9a1e76d4241
francis <willenerfrancis0@gmail.com>
2022-12-03 20:58:46 -0300

    Deploy

--- src/main/java/com/portafolio/SpringBoot/Security/Controller/AuthController.java
+++ src/main/java/com/portafolio/SpringBoot/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://franciswillener-ap.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
